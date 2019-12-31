59149eedafc74453a9adb76969e4bc5eb6eba8c8
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2019-12-31 12:04:29 +0100

    V 1.0.3
    Former-commit-id: 12653f33e5eb85b3bcfefdb950a6a4e09e505465

--- main.js
+++ main.js
@@ -65,10 +65,10 @@ ipc.on('print-to-pdf', event => {
 
 
 autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+  win.webContents.send('update_available');
 });
 autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+  win.webContents.send('update_downloaded');
 });
 
 
