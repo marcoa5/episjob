6c44366bac8e7555380d500a1f9b825f9b66cd13
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2020-01-03 22:49:14 +0100

    V 1.0.24

--- JS/command.js
+++ JS/command.js
@@ -537,23 +537,14 @@ function aprifile(a){
 		var filename =  dialog.showOpenDialogSync(options, "");
 		if(filename!==undefined){
         $.get(filename, function(data) {document.getElementById('salva').innerHTML = data})}
-		aggiungicheckmail()
+		
 		closeMenu()
 	} else {
 		$.get(a, function(data) {
         document.getElementById('salva').innerHTML = data
-		aggiungicheckmail()
         })
 	}
 }
-
-function aggiungicheckmail(){
-	var input = document.getElementById("indmail");
-	input.addEventListener("keydown", function(event) {if (event.keyCode === 13) {event.preventDefault(); nuovamail();}});
-}
-
-
-
     function pulisci(){
         document.getElementById('data2').value="";
         var filename =  "blank.ma"
