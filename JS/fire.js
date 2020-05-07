e5784b461e2381724717a57c720ad00cd45a3ca5
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2020-05-07 11:23:46 +0200

    V 1.2.0 firebase

---
+++ JS/fire.js
@@ -0,0 +1,38 @@
+var firebase = require('firebase');
+var storage = require('firebase/firebase-storage');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCKS9waoMAR6NjpDZIMeaL4GezqqGgvxRs",
+  authDomain: "epi-s-job.firebaseapp.com",
+  databaseURL: "https://epi-s-job.firebaseio.com",
+  projectId: "epi-s-job",
+  storageBucket: "epi-s-job.appspot.com",
+  messagingSenderId: "32439813654",
+  appId: "1:32439813654:web:3d930228f8509fe2fb1737"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function aggiornatech(){
+  firebase.storage().ref('techupd.txt').getDownloadURL().then(function(url) {
+    $.get(url, (data)=> {
+			fs.writeFileSync(process.argv[5].substring(11) + "\\tech.txt", data)
+		})
+  });
+}
+
+function aggiornamol(){
+  firebase.storage().ref('molupd.txt').getDownloadURL().then(function(url) {
+    $.get(url, (data)=> {
+			fs.writeFileSync(process.argv[5].substring(11) + "\\mol.txt", data)
+		})
+  });
+}
+
+function aggiornacli(){
+  firebase.storage().ref('customersupd.txt').getDownloadURL().then(function(url) {
+    $.get(url, (data)=> {
+			fs.writeFileSync(process.argv[5].substring(11) + "\\customers.txt", data)
+		})
+  });
+}
