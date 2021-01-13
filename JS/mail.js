16812d8c0e3026f792f88953239ee12e7667af06
marcoa5 <marco.arato@gmail.com>
2021-01-13 12:22:35 +0100

    realtime

--- JS/mail.js
+++ JS/mail.js
@@ -26,8 +26,9 @@ async function contEconf(){
 			if(pathfs.extname(file)=='.econf'){
 				var nome = file.substring(0,file.length - 6)
 				var nomeL = path + '\\' + nome
-				var refpdf = firebase.storage().ref().child(require("os").userInfo().username + '/' + nome + '.pdf')
-				var refma = firebase.storage().ref().child(require("os").userInfo().username +'/' + nome + '.ma')
+				var user = `${user.Nome} ${user.Cognome}`
+				var refpdf = firebase.storage().ref().child(user + '/' + nome + '.pdf')
+				var refma = firebase.storage().ref().child(user +'/' + nome + '.ma')
 				fetch(nomeL + '.pdf')
 				.then((a)=>{
 					a.blob().then(b=>{
