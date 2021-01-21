4cdf00a5813f9503429877b16fba134a07f3533f
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-01-21 11:19:59 +0100

    maillist

--- JS/fire.js
+++ JS/fire.js
@@ -133,7 +133,9 @@ function readRealTimeDB(id, eMail){
     v.Mail = eMail
     await require('fs').writeFileSync(path, JSON.stringify(v))
     firebase.default.database().ref(`mails/${v.Nome} ${v.Cognome}`).once('value', async mailList=>{
-      if(mailList.val()!=null) await require('fs').writeFileSync(path1,mailList.val())
+      if(mailList.val()!=null) {
+        await require('fs').writeFileSync(path1,mailList.val())
+      }
     })
     readConf()
   })
