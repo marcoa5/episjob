c7d39221fb77b6103b3b42c78c05b2a3610c9bd2
marcoa5 <marco.arato@gmail.com>
2021-05-24 16:56:12 +0200

    k

--- JS/command.js
+++ JS/command.js
@@ -1576,6 +1576,6 @@ function uplSave(){
 	p.firmacc1=''
 	p.firmatt1=''
 	console.log(p)
-	p.filename = getSavedName()
+	p.filename = getSavedName() + '.pdf'
 	if($('#matricola').text()!='' && getDaySave()!=0)	firebase.default.database().ref('Saved/' + $('#matricola').text() + '/' + getDaySave()).set(p)
 }
